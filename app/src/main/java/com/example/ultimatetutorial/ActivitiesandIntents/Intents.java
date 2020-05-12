6a76ee946f48c08d5069a0f4fbb3b9dfb3f37e39
janewaitara <janewaitara99@gmail.com>
2020-05-12 18:47:53 +0300

    implicit intents -made a module a default dialer

--- app/src/main/java/com/example/ultimatetutorial/ActivitiesandIntents/Intents.java
+++ app/src/main/java/com/example/ultimatetutorial/ActivitiesandIntents/Intents.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ultimatetutorial.R;
 
@@ -17,5 +18,11 @@ public class Intents extends AppCompatActivity {
         Intent secondIntent = new Intent(Intents.this, SecondActivity.class);
         secondIntent.putExtra(Intent.EXTRA_TEXT, "This is from Intents Activity");
         startActivity(secondIntent);
+    }
+
+    public void onButtonClick(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_DIAL);
+        Intents.this.startActivity(sendIntent);
     }
 }
