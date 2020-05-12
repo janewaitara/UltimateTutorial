34344a1bdecb178bb39128df0f362595e17947e2
janewaitara <janewaitara99@gmail.com>
2020-05-12 03:28:15 +0300

    Using Log messages to show the Activity Lifecycles

---
+++ app/src/main/java/com/example/ultimatetutorial/FirstActivity.java
@@ -0,0 +1,48 @@
+package com.example.ultimatetutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class FirstActivity extends AppCompatActivity {
+    private static final String  TAG = FirstActivity.class.toString();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Log.d(TAG , "in onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG , "in onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG , "in onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG , "in onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG , "in onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG , "in onDestroy");
+    }
+}
